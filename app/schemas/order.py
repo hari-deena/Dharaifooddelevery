2186896bd8c7e5e291ee0f8e91bc1f931825f73b
hari-deena <harideena.l@justlogix.com>
2025-11-11 00:07:14 +0530

    Comment out user_id field in OrderPayloadre
    Commented out the user_id field in OrderPayload class.

--- app/schemas/order.py
+++ app/schemas/order.py
@@ -8,7 +8,7 @@ class Item(BaseModel):
     quantity: int = Field(gt=0, description="Must be a positive integer")
 
 class OrderPayload(BaseModel):
-    user_id: int = Field(gt=0, description="Must be a positive integer")
+    # user_id: int = Field(gt=0, description="Must be a positive integer")
     total_amount: float = Field(ge=0, description="Must be a non-negative number (e.g., 0 or positive)")
     items: List[Item] = Field(min_length=1, description="At least one item is required")
     
